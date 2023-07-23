30fe5a380ead089e6cad9c31cece7ff9fae50e56
Arysill <125421389+Arysill@users.noreply.github.com>
2023-07-23 17:40:10 +0000

    Deploying to gh-pages from @ Arysill/goit-js-hw-09@883d923483d7c4dd30d1085c55c7eb654e9b114b 🚀

--- 01-color-switcher.3bd7180c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={start:document.querySelector("[data-start"),stop:document.querySelector("[data-stop")};t.stop.disabled=!0,t.start.addEventListener("click",onBtnClickStart),t.stop.addEventListener("click",onBtnClickStop)}();
-//# sourceMappingURL=01-color-switcher.3bd7180c.js.map
--- 01-color-switcher.6f3af14a.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("[data-start"),stop:document.querySelector("[data-stop")};t.stop.disabled=!0,t.start.addEventListener("click",onBtnClickStart),t.stop.addEventListener("click",onBtnClickStop);
-//# sourceMappingURL=01-color-switcher.6f3af14a.js.map
